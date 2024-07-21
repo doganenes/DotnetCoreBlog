73351c9e3cfb599e1c78edf6fa7b42a99c64fce8
Enes Doğan <enesdoganlive@hotmail.com>
2024-07-21 21:12:22 +0300

    viewbag son blog eklendi

--- CoreBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
+++ CoreBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreBlog.BusinessLayer.Concrete;
+using CoreBlog.DataAccessLayer.Concrete;
+using CoreBlog.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.UI.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic2 : ViewComponent
     {
+        Context c = new Context();
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.TGetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            ViewBag.v4 = c.Blogs.OrderByDescending(x=> x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
     }
 }
