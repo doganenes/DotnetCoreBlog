df4d55d08ea43c6ac14da44c879ced15e297537d
Enes Doğan <enesdoganlive@hotmail.com>
2024-08-04 22:17:17 +0300

    identity updated

--- CoreBlog.UI/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreBlog.UI/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using CoreBlog.BusinessLayer.Concrete;
 using CoreBlog.DataAccessLayer.Concrete;
 using CoreBlog.DataAccessLayer.EntityFramework;
+using CoreBlog.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.UI.ViewComponents.Writer
@@ -8,10 +10,23 @@ namespace CoreBlog.UI.ViewComponents.Writer
     public class WriterAboutOnDashboard : ViewComponent
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        private readonly UserManager<AppUser> _userManager;
+
         Context c = new Context();
-        public IViewComponentResult Invoke()
+
+        public WriterAboutOnDashboard(UserManager<AppUser> userManager)
         {
-            var usermail = User.Identity.Name;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                
+            var username = User.Identity.Name;
+            ViewBag.v = username;
+            var usermail = c.Users.Where(x => x.UserName ==  username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = wm.GetWriterById(writerID);
             return View(values);
